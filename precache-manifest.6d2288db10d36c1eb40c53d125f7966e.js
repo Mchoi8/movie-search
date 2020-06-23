374845a674086f6c81174ea63d73698a2eb410a5
mycinus <45410115+mycinus@users.noreply.github.com>
2020-06-22 22:36:22 -0700

    Updates

---
+++ precache-manifest.6d2288db10d36c1eb40c53d125f7966e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4741e48ca6110d711968237d48847060",
+    "url": "/Mchoi8/movie-search/index.html"
+  },
+  {
+    "revision": "29d6693bdc2abc34b358",
+    "url": "/Mchoi8/movie-search/static/css/main.408bf84b.chunk.css"
+  },
+  {
+    "revision": "1d5176d7069b1eeec081",
+    "url": "/Mchoi8/movie-search/static/js/2.a68a625f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mchoi8/movie-search/static/js/2.a68a625f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d6693bdc2abc34b358",
+    "url": "/Mchoi8/movie-search/static/js/main.643eb35c.chunk.js"
+  },
+  {
+    "revision": "007fc785339b1ae05cc6",
+    "url": "/Mchoi8/movie-search/static/js/runtime-main.b8d4591a.js"
+  }
+]);
